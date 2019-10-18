50c995d2409d93c9f5831c6f2dac4feadc35be3c
Hugo <hugovk@users.noreply.github.com>
2019-10-18 09:14:15 +0300

    Python 3.8 is out

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
