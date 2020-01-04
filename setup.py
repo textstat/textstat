9c6eded93638217c6ffb2c3d52e0ea8c704b1e75
Alex Ward <alxwrd@googlemail.com>
2020-01-04 21:34:49 +0000

    version bump to 0.6.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from io import open
 setup(
     name='textstat',
     packages=['textstat'],
-    version='0.5.7',
+    version='0.6.0',
     description='Calculate statistical features from text',
     author='Shivam Bansal, Chaitanya Aggarwal',
     author_email='shivam5992@gmail.com',
--- textstat/__init__.py
+++ textstat/__init__.py
@@ -1,7 +1,7 @@
 from .textstat import textstat
 
 
-__version__ = (0, 5, 7)
+__version__ = (0, 6, 0)
 
 
 for attribute in dir(textstat):
