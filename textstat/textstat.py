4647a238b79c8f20938a1d1180cb816e6740a595
LKirst <lk_tech@mailbox.org>
2021-09-06 15:26:29 +0200

    Add docstring to is_difficult_word

--- textstat/textstat.py
+++ textstat/textstat.py
@@ -710,7 +710,28 @@ class textstatistics:
         return diff_words
 
     @lru_cache(maxsize=128)
-    def is_difficult_word(self, word, syllable_threshold=2):
+    def is_difficult_word(self, word: str, syllable_threshold: int=2):
+        """Return True if `word` is a difficult word.
+
+        The function checks if if the word is in the Dale-Chall list of
+        easy words. However, it currently doesn't check if the word is a
+        regular inflection of a word in the Dale-Chall list!
+
+        Parameters
+        ----------
+        word : str
+            A word.
+        syllable_threshold : int, optional
+            Minimum number of syllables a difficult word must have. The
+            default is 2.
+
+        Returns
+        -------
+        bool
+            True if the word is not in the easy words list and is longer than
+            `syllable_threshold`; else False.
+
+        """
         easy_word_set = self.__get_lang_easy_words()
         if word in easy_word_set:
             return False
