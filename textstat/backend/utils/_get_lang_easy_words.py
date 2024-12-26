10da3bd1843b09a95fc2b48b6e7695267589c8ce
Sean Hastings <seanqn7@gmail.com>
2024-12-25 21:55:41 -0500

    attempted to make get_lang_easy_words compatible with python 3.7 and 3.8 again

--- textstat/backend/utils/_get_lang_easy_words.py
+++ textstat/backend/utils/_get_lang_easy_words.py
@@ -1,28 +1,41 @@
 from __future__ import annotations
 
-import importlib.resources as importlib_resources
 import warnings
 
 from ._typed_cache import typed_cache
 from ._get_lang_root import get_lang_root
+import sys
+
+if sys.version_info < (3, 10):
+    import pkg_resources
+
+    def _set_words(lang_root: str) -> set[str]:
+        return {
+            ln.decode("utf-8").strip()
+            for ln in pkg_resources.resource_stream(
+                "textstat",
+                f"resources/{lang_root}/easy_words.txt",
+            )
+        }
+else:
+    import importlib.resources as importlib_resources
+
+    def _set_words(lang_root: str) -> set[str]:
+        ref = importlib_resources.files("textstat").joinpath(
+            f"resources/{lang_root}/easy_words.txt"
+        )
+        with ref.open() as f:
+            return {ln.strip() for ln in f}
 
 
 @typed_cache
 def get_lang_easy_words(lang: str) -> set[str]:
     lang_root = get_lang_root(lang)
     try:
-        ref = importlib_resources.files("textstat").joinpath(
-            f"resources/{lang_root}/easy_words.txt"
-        )
-        with ref.open() as f:
-            return {ln.strip() for ln in f}
+        return _set_words(lang_root)
     except FileNotFoundError:
         warnings.warn(
             "There is no easy words vocabulary for " f"{lang_root}, using english.",
             Warning,
         )
-        ref = importlib_resources.files("textstat").joinpath(
-            "resources/en/easy_words.txt"
-        )
-        with ref.open() as f:
-            return {ln.strip() for ln in f}
+        return _set_words("en")
